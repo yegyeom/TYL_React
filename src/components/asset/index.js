216d72c83160899e520f5d410f60c074fc66e01b
yegyeom <reallyy1111@gmail.com>
2021-08-15 21:08:36 +0900

    [feat] 자산 페이지 제작 및 기본 구성

---
+++ src/components/asset/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const index = () => {
+  return (
+    <>
+      <div className="total-asset-container">
+        <ul className="asset-box">
+          <li> 총 자산 </li>
+          <li id="right">8,500,100 TYL</li>
+        </ul>
+      </div>
+      <div className="graph-container"></div>
+    </>
+  );
+};
+
+export default index;
