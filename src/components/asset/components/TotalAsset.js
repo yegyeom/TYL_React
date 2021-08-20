09743588266c901104cb2d9a01afdedd59d51e24
yegyeom <reallyy1111@gmail.com>
2021-08-20 20:56:23 +0900

    [feat] 내 자산 초기화면 구현

---
+++ src/components/asset/components/TotalAsset.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../../styles/sass/main.css';
+
+const TotalAsset = () => {
+  return (
+    <div className="total-asset-container">
+      <ul className="total-asset-box">
+        <li> 총 자산 </li>
+        <li id="right">8,500,100 TYL</li>
+      </ul>
+    </div>
+  );
+};
+
+export default TotalAsset;
--- src/components/asset/index.js
+++ src/components/asset/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
-
+import TotalAsset from './components/TotalAsset.js';
+import AssetGraph from './components/AssetGraph.js';
+import AssetList from './components/AssetList.js';
 const index = () => {
   return (
     <>
-      <div className="total-asset-container">
-        <ul className="asset-box">
-          <li> 총 자산 </li>
-          <li id="right">8,500,100 TYL</li>
-        </ul>
-      </div>
-      <div className="graph-container"></div>
+      <TotalAsset />
+      <AssetGraph />
+      <AssetList />
     </>
   );
 };
