3bcb74db5eb784587b72bf92c1ea6dc63a4378c4
yegyeom <reallyy1111@gmail.com>
2021-08-22 03:44:32 +0900

    [feat] 하단 메뉴박스 사용자탭 추가

--- src/components/navigation/Page.js
+++ src/components/navigation/Page.js
@@ -1,21 +1,36 @@
+import { useEffect, useState } from 'react';
 import { Route, NavLink } from 'react-router-dom';
 import Home from '../home/index';
 import Investment from '../investment/index';
 import Ranking from '../ranking/index';
 import Asset from '../asset/index.js';
 import Profile from '../profile/index.js';
+import Auth from '../auth/index.js';
 import '../../styles/sass/main.css';
+import { useSelector } from 'react-redux';
+import { checkValidity, selectUser } from '../auth/userSlice';
 import MyAsset from '../asset/components/MyAsset';
+import profile_img from '../../styles/images/profile_img.png';
 
 const Page = () => {
+  const validity = useSelector(checkValidity);
+  const user = useSelector(selectUser);
+  const [bottomMenuBox, setBottomMenuBox] = useState([]);
+
+  useEffect(() => {
+    if (validity) {
+      setBottomMenuBox([
+        { link: '', title: user.nickname },
+        { link: '/Asset', title: '내 자산' },
+        { link: '/Profile', title: '내 정보' },
+      ]);
+    } else setBottomMenuBox([{ link: '/login', title: '로그인' }]);
+  }, [validity]);
+
   const topMenuBox = [
     { link: '/', title: '홈' },
     { link: '/Investment', title: '투자' },
     { link: '/Ranking', title: '랭킹' },
-  ];
-  const bottomMenuBox = [
-    { link: '/Asset', title: '내 자산' },
-    { link: '/Profile', title: '내 정보' },
   ];
 
   const topMenuList = topMenuBox.map((menu, idx) => (
@@ -26,18 +41,32 @@ const Page = () => {
     </li>
   ));
 
-  const bottomMenuList = bottomMenuBox.map((menu, idx) => (
-    <li key={idx}>
-      <NavLink exact to={menu.link} activeClassName="active">
-        {menu.title}
-      </NavLink>
-    </li>
-  ));
+  const bottomMenuList = bottomMenuBox.map((menu, idx) => {
+    if (menu.title != '내 자산' && menu.title != '내 정보' && menu.title != '로그인') {
+      return (
+        <li className="user-profile">
+          <div className="profile-img-box">
+            <img src={profile_img} className="profile" />
+          </div>
+          <td className="user-name">{menu.title}</td>
+          <td className="user-ment">님, 반가워요!</td>
+        </li>
+      );
+    }
+    return (
+      <li key={idx}>
+        <NavLink exact to={menu.link} activeClassName="active">
+          {menu.title}
+        </NavLink>
+      </li>
+    );
+  });
 
   return (
     <div className="page">
       <div className="menu-bar">
         <h2>LOGO</h2>
+        <h4>{user.nickname}</h4>
         <ul className="tabs">{topMenuList}</ul>
         <ul className="tabs">{bottomMenuList}</ul>
       </div>
@@ -48,6 +77,7 @@ const Page = () => {
         <Route path="/Ranking" component={Ranking} />
         <Route exact path="/Asset" component={Asset} />
         <Route path="/Profile" component={Profile} />
+        <Route path="/login" component={Auth} />
         <Route path="/Asset/stock" component={MyAsset} />
         <Route path="/Asset/btc" component={MyAsset} />
       </div>
