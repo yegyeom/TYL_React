94cc07e685f2b30c69239ff6de0e75bdaf6121cd
yegyeom <reallyy1111@gmail.com>
2021-08-26 03:08:13 +0900

    [feat] 내 자산 종목별 상세 주문 내역 구현

--- src/components/asset/components/presentational/Modal.js
+++ src/components/asset/components/presentational/Modal.js
@@ -3,7 +3,28 @@ import '../../../../styles/sass/main.css';
 
 const Modal = props => {
   // 열기, 닫기, 모달 헤더 텍스트를 부모로부터 받아옴
-  const { open, close, header, onAccept } = props;
+  const { open, close, header, onAccept, TradeBox } = props;
+
+  const TradeList = TradeBox.map(menu =>
+    menu.title === header
+      ? menu.order.map((list, idx) => (
+          <div className="detail-trade-list" key={idx}>
+            <ul className="detail-trade-left">
+              <li style={list.trading === '매수' ? { color: 'red' } : { color: 'blue' }}>
+                {list.trading}
+              </li>
+              <li style={{ paddingTop: '0px' }}>{list.date}</li>
+            </ul>
+            <ul className="detail-trade-right">
+              <li>{list.total.toLocaleString('ko-KR')} TYL</li>
+              <li style={{ paddingTop: '0px' }}>
+                {list.qty}주 {list.one.toLocaleString('ko-KR')} TYL
+              </li>
+            </ul>
+          </div>
+        ))
+      : null,
+  );
 
   return (
     // 모달이 열릴때 openModal 클래스가 생성된다.
@@ -16,7 +37,7 @@ const Modal = props => {
               &times;
             </button>
           </header>
-          {props.children}
+          {TradeList}
           <footer></footer>
         </section>
       ) : null}
