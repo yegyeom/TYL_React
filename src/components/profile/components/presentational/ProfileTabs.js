ab3fab7757c782749e1d580eb9df71997eaddce0
yegyeom <reallyy1111@gmail.com>
2021-08-26 17:51:13 +0900

    [feat] 닉네임 변경 모달 구현 (임시)

--- src/components/profile/components/presentational/ProfileTabs.js
+++ src/components/profile/components/presentational/ProfileTabs.js
@@ -1,9 +1,5 @@
 import React, { useState } from 'react';
-import Home from '../../../home/index.js';
-import Investment from '../../../investment/index';
-import Ranking from '../../../ranking/index';
 import '../../../../styles/sass/main.css';
-import { Route, Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import Modal from './Modal.js';
 import { logout } from '../../../auth/userSlice';
@@ -28,9 +24,27 @@ const ProfileTabs = ({ url }) => {
   };
 
   const openModal = e => {
-    e.target.id === 'logout-button'
-      ? setModalData({ header: '로그아웃', text: '로그아웃하시겠습니까?', onAccept: onLogout })
-      : setModalData({ header: '회원탈퇴', text: '탈퇴하시겠습니까?', onAccept: closeModal });
+    if (e.target.id === 'logout-button')
+      setModalData({
+        id: 'out',
+        header: '로그아웃',
+        text: '로그아웃하시겠습니까?',
+        onAccept: onLogout,
+      });
+    else if (e.target.id === 'delete-button')
+      setModalData({
+        id: 'out',
+        header: '회원탈퇴',
+        text: '탈퇴하시겠습니까?',
+        onAccept: closeModal,
+      });
+    else if (e.target.id === 'nickname-button')
+      setModalData({
+        id: 'modify',
+        header: '닉네임 변경',
+        text: '한글, 영어, 숫자 조합 2-6자리',
+        onAccept: closeModal,
+      });
     setModalOpen(true);
   };
 
@@ -38,12 +52,10 @@ const ProfileTabs = ({ url }) => {
     <>
       <div className="profile-tabs">
         <ul id="profile-menu" className="tabs">
-          <li>
-            <Link to={`${url}/nickname`}>닉네임 변경</Link>
+          <li id="nickname-button" onClick={openModal}>
+            닉네임 변경
           </li>
-          <li>
-            <Link to={`${url}/photo`}>프로필 사진 변경</Link>
-          </li>
+          <li>프로필 사진 변경</li>
           <li id="logout-button" onClick={openModal}>
             로그아웃
           </li>
@@ -55,16 +67,11 @@ const ProfileTabs = ({ url }) => {
             close={closeModal}
             onAccept={modalData.onAccept}
             header={modalData.header}
+            id={modalData.id}
           >
             {modalData.text}
           </Modal>
         </ul>
-
-        <div>
-          <Route path="/" component={Home} exact />
-          <Route path="/${url}/nickname" component={Investment} />
-          <Route path="/${url}/photo" component={Ranking} />
-        </div>
       </div>
     </>
   );
