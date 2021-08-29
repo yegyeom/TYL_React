2022da19b1985c018dd7717a14fb38be2148619c
yegyeom <reallyy1111@gmail.com>
2021-08-29 20:20:24 +0900

    [feat] 닉네임 변경 모달창 길이 제한

--- src/components/profile/components/presentational/Modal.js
+++ src/components/profile/components/presentational/Modal.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../../../styles/sass/main.css';
 
 const Modal = props => {
   // 열기, 닫기, 모달 헤더 텍스트를 부모로부터 받아옴
   const { open, close, header, onAccept, id } = props;
 
+  const handleChangeInput = event => {
+    //   console.log(event.target.value);
+    setNickname(event.target.value);
+  };
+
+  const [nickname, setNickname] = useState('');
+
   if (id === 'out') {
+    //로그아웃, 탈퇴
     return (
-      // 모달이 열릴때 openModal 클래스가 생성된다.
       <div className={open ? 'openModal modal' : 'modal'}>
         {open ? (
           <section>
@@ -32,11 +39,11 @@ const Modal = props => {
       </div>
     );
   } else {
+    //닉네임, 프로필 사진 변경
     return (
-      // 모달이 열릴때 openModal 클래스가 생성된다.
       <div className={open ? 'openModal modal' : 'modal'}>
         {open ? (
-          <section style={{ width: '300px', height: '180px' }}>
+          <section style={{ width: '300px', height: '195px' }}>
             <header style={{ paddingLeft: '60px' }}>
               {header}
               <button className="close" onClick={close}>
@@ -44,10 +51,26 @@ const Modal = props => {
               </button>
             </header>
             <main id="modify">
-              <input className="modal-input" placeholder={props.children} />
+              <input
+                type="text"
+                className="modal-input"
+                placeholder={props.children}
+                onChange={handleChangeInput}
+                maxLength="6"
+                autoFocus
+              />
+              {nickname.length > 1 ? (
+                <div className="check-nickname">사용 가능한 닉네임입니다!</div>
+              ) : (
+                <div className="check-nickname">&nbsp;</div>
+              )}
             </main>
             <footer>
-              <button id="modify" onClick={onAccept}>
+              <button
+                className="modify"
+                onClick={nickname.length > 1 ? onAccept : null}
+                id={nickname.length > 1 ? 'possible' : null}
+              >
                 완료
               </button>
             </footer>
