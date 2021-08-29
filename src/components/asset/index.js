f18c623bdc1d3857ba8be0b88e247edac7424b73
yegyeom <reallyy1111@gmail.com>
2021-08-29 18:14:03 +0900

    [chore] 내 자산 그래프 임시 데이터 수정

--- src/components/asset/index.js
+++ src/components/asset/index.js
@@ -2,16 +2,6 @@ import React from 'react';
 import AssetConatiner from './components/container/AssetContainer.js';
 
 const index = () => {
-  const validity = useSelector(checkValidity);
-  const [asset, setAsset] = useState(0);
-
-  useEffect(() => {
-    if (validity)
-      axios.get('asset').then(res => {
-        setAsset(res.data.asset);
-      });
-  }, [validity]);
-
   return (
     <>
       <AssetConatiner />
