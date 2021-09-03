c24eb4a874d87788587aeafa224e6caee8fbfe97
utturi <52422925+utturi@users.noreply.github.com>
2021-09-04 02:34:48 +0900

    [refactor] 랭킹 탭 실시간 데이터 반영 및 디자인 수정 (#28)
    * [feat] 홈 탭 구현 (#9)
    
    *  [feat] 홈 탭의 자산 순위 구현
    * [chore] home 디렉토리 구조 세팅
    
    * [refactor] 홈 화면 오류 및 디자인 수정 (#11)
    
    * [refactor] 홈 코드 및 디자인 수정
    
    * [chore] home.scss 디자인 수정
    
    * [fix] 홈 화면 오류 수정
    
    * [feat] 홈탭 전날 유저 정보 박스 Ticker 애니메이션 구현
    
    * [chore] react-spring 패키지 설치
    
    * [chore] 홈 디렉토리 파일 구조 정리
    
    * [refactor] 홈 디자인 변경
    
    * [refactor] 홈 유저랭킹 Ticker 구현
    
    * [chore] box-shadow 일부 변경
    
    * [fetch] PR #19
    
    * [feat] 랭킹 탭 카테고리 구현
    
    * [chore] 폴더 구조 변경
    
    * [refactor] 랭킹 탭 카테고리 구조 변경
    
    * [refactor] 이미지 추가 및 디자인 변경
    
    * [feat] 랭킹 검색 폼 구현
    
    * [feat] 랭킹 리스트 구현
    
    * [chore] react-select 패키지 설치
    
    * [feat] 랭킹 select 구현
    
    * [fix] ranking.scss 코드 수정
    
    * [refactor] 랭킹 탭 디자인 수정
    
    * [refactor] selecter별 실시간 데이터 구현
    
    * [fetch] PR #27
    
    Co-authored-by: 임대호 <eogh0959@gmail.com>

--- src/components/ranking/component/container/AllTab.js
+++ src/components/ranking/component/container/AllTab.js
@@ -3,36 +3,46 @@ import axios from 'axios';
 import AllRank from '../presentational/AllRank';
 
 const AllTab = () => {
-  const [rank, setRank] = useState([]);
+  const [inProgress, setInProgress] = useState(true);
+  const [assetRank, setAssetRank] = useState([]);
+  const [profitRank, setProfitRank] = useState([]);
 
   useEffect(() => {
     axios.get('rank/asset').then(res => {
-      console.log(res);
-      //setRank(res.data.rank);
+      setAssetRank(res.data.rank);
+      setInProgress(false);
+      //console.log(res.data.rank);
     });
   }, []);
 
-  const assetList = [
-    { rank: '1위', nk_name: 'KIM', asset: '400,000,000틸' },
-    { rank: '2위', nk_name: 'LEE', asset: '300,000,000틸' },
-    { rank: '3위', nk_name: 'LIM', asset: '200,000,000틸' },
-    { rank: '4위', nk_name: 'SON', asset: '80,000,000틸' },
-    { rank: '5위', nk_name: 'YUN', asset: '50,000,000틸' },
-    { rank: '6위', nk_name: 'PARK', asset: '10,000,000틸' },
-  ];
+  useEffect(() => {
+    axios.get('rank/preday-history').then(res => {
+      setProfitRank(res.data.upperRank);
+      setInProgress(false);
+      //console.log(res.data.upperRank);
+    });
+  }, []);
 
-  const yieldList = [
-    { rank: '1위', nk_name: '안녕', asset: '48.3%' },
-    { rank: '2위', nk_name: '주식부자', asset: '39.2%' },
-    { rank: '3위', nk_name: '호호', asset: '36.1%' },
-    { rank: '4위', nk_name: '임대호', asset: '29.1%' },
-    { rank: '5위', nk_name: '쀼시닝', asset: '25.8%' },
-    { rank: '6위', nk_name: '시계나이', asset: '12.4%' },
-  ];
+  if (inProgress) {
+    return <div></div>;
+  }
+
+  // json add data(ranking)
+  for (let i = 0; i < assetRank.length; i++) {
+    assetRank[i].ranking = i + 1;
+  }
+
+  for (let i = 0; i < profitRank.length; i++) {
+    profitRank[i].ranking = i + 1;
+  }
+
+  for (let i = 0; i < assetRank.length; i++) {
+    assetRank[i].asset = assetRank[i].asset.toLocaleString();
+  }
 
   return (
     <>
-      <AllRank ainfo={assetList} yinfo={yieldList} />
+      <AllRank ainfo={assetRank} yinfo={profitRank} />
     </>
   );
 };
