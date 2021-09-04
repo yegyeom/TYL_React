c8d756f2fc7f1a06d6ad4f15f3902858134ee815
yegyeom <reallyy1111@gmail.com>
2021-09-05 04:38:48 +0900

    [style] 프로필 사진 테두리 적용

--- src/components/asset/components/presentational/AssetList.js
+++ src/components/asset/components/presentational/AssetList.js
@@ -6,10 +6,10 @@ const AssetList = ({ AssetBox, CashBox, match }) => {
   const AssetList = AssetBox.map((menu, idx) => (
     <li key={idx}>
       <Link to={`${match.url}/${menu.link}`}>
-        <div style={{ width: '65px' }}>
+        <div style={{ width: '50px', paddingTop: '5px' }}>
           <img src={menu.img} width="100%" />
         </div>
-        <div className="gang" style={{ paddingLeft: '10px' }}>
+        <div className="gang" style={{ paddingLeft: '0px' }}>
           <ul className="asset-tabs-left">
             <li>{menu.title}</li>
             <li>{menu.total.toLocaleString('ko-KR')} TYL</li>
@@ -42,10 +42,10 @@ const AssetList = ({ AssetBox, CashBox, match }) => {
       <div className="asset-list-container">
         <ul id="asset">
           <li className="list-cash">
-            <div style={{ width: '65px' }}>
+            <div style={{ width: '50px', paddingTop: '5px' }}>
               <img src={CashBox.img} width="100%" />
             </div>
-            <div className="gang" style={{ paddingLeft: '10px' }}>
+            <div className="gang" style={{ paddingLeft: '0px' }}>
               <ul className="asset-tabs-left">
                 <li>{CashBox.title}</li>
                 <li>{CashBox.total.toLocaleString('ko-KR')} TYL</li>
